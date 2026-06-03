d2d42cfadb09f7d15caba70e1004bc84f78f36eb
dullee <91831552+dullee@users.noreply.github.com>
2026-06-03 17:26:30 +0800

    a

--- app/page.js
+++ app/page.js
@@ -1,65 +1,54 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
+    <div className="flex flex-col flex-1 items-center bg-[rgba(243,244,246,1)] font-sans ">
+      <main className="flex  flex-col items-center justify-between py-32 px-16 sm:items-start">
+        <div className="flex flex-col items-center gap-6 bg-white py-4 px-6 rounded-md shadow-md text-center sm:text-left">
+          <h1 className="max-w-xs text-xl font-semibold leading-10 tracking-tight text-black text-center">
+            To-do list
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <div className=" flex gap-[6px]">
+            <input
+              id="input"
+              className="bg-white text-black border-solid border-1 rounded-md border-[rgba(228,228,231,1)]"
+              placeholder="Add a new task..."
+            ></input>
+            <button className="bg-[rgba(60,130,246,1)] p-1 rounded-md">
+              Add
+            </button>
+          </div>
+          <div className="flex gap-[6px] self-start ">
+            <button className="bg-[rgba(60,130,246,1)] p-1 rounded-md">
+              All
+            </button>
+            <button className="bg-[rgba(243,244,246,1)] text-[rgba(54,54,54,1)]  p-1 rounded-md">
+              Active
+            </button>
+            <button className="bg-[rgba(243,244,246,1)] text-[rgba(54,54,54,1)] p-1 rounded-md">
+              Completed
+            </button>
+          </div>
         </div>
       </main>
     </div>
   );
 }
+// const input = useRef("input");
+// const addButton = document.querySelector("button");
+// const ul = document.getElementById("ulElement");
+
+// let tasks = [];
+
+// const addTask = () => {
+//   if (input.value) {
+//     for (let i = 0; i < tasks.length; i++) {
+//       tasks[i] == input.value;
+//       console.log("task already exists");
+//       return;
+//     }
+//     tasks.push({ task: input.value, completed: false });
+
+//     ul.appendChild;
+//     console.log(tasks);
+//   }
+// };
+// addButton.addEventListener("click", addTask);
