6f666186e2c221a5a284a0b5482ea2145b087a7b
dullee <91831552+dullee@users.noreply.github.com>
2026-06-08 13:28:49 +0800

    3

--- app/_components/list.js
+++ app/_components/list.js
@@ -2,33 +2,8 @@
 import { useState } from "react";
 
 export default function List() {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      text: "task 1",
-    },
-    { id: 2, text: "task 2" },
-    { id: 3, text: "task 2" },
-    { id: 4, text: "task 2" },
-    { id: 5, text: "task 2" },
-    { id: 6, text: "task 2" },
-  ]);
 
-  return (
-    <div className="flex flex-col w-full gap-2.5">
-      {data.map((task) => (
-        <div
-          className="flex text-black p-4 w-full  bg-[rgba(249,250,251,1)] rounded-md items-center"
-          key={task.id}
-        >
-          <div className="flex flex-row items-center gap-2.5">
-            <div className="w-5 h-5 bg-white border-[rgba(118,118,118,1)] border rounded-xs"></div>
-            {task.text}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+
   //   data.map((task) => (
   //     <div
   //       key={task.id}
--- app/page.js
+++ app/page.js
@@ -3,15 +3,46 @@ import { useState } from "react";
 import List from "./_components/list.js";
 
 export default function Home() {
-  const [number, setNumber] = useState(0);
-  const handClick = () => {
-    setNumber(number + 1);
+  const [filter, setFilter] = useState("all");
+  const changeFilter = (e) => {
+    setFilter(e);
   };
 
-  const [pressedButton, setPressedButton] = useState("all");
-  const pressButton = (button) => {
-    setPressedButton(button);
+  const [inputValue, setInputValue] = useState("");
+
+  const [data, setData] = useState([
+    { id: 1, text: "task 1", completed: true },
+    // { id: 2, text: "task 2" },
+    // { id: 3, text: "task 2" },
+    // { id: 4, text: "task 2" },
+    // { id: 5, text: "task 2" },
+    // { id: 6, text: "task 2" },
+  ]);
+
+  const addTask = () => {
+    if (!inputValue.trim()) return;
+
+    const newTask = { id: Date.now(), text: inputValue, completed: false };
+    setData([...data, newTask]);
   };
+
+  const removeTask = (taskId) => {
+    const updatedTasks = data.filter((task) => {
+      task && task.id !== taskId;
+    });
+    setData(updatedTasks);
+    console.log(data);
+  };
+
+  const filteredTasks = data?.filter((task) => {
+    if (filter === "active") {
+      return !task.completed;
+    }
+    if (filter === "completed") {
+      return task.completed;
+    }
+    return true;
+  });
 
   return (
     <div className="flex flex-col flex-1 items-center bg-[rgba(243,244,246,1)] font-sans ">
@@ -20,44 +51,71 @@ export default function Home() {
           <h1 className="max-w-xs text-xl font-semibold leading-10 tracking-tight text-black text-center">
             To-do list
           </h1>
-          <button onClick={handClick}>add</button>
-          <p>{number}</p>
+
           <div className=" flex gap-1.5">
             <input
               id="input"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
               className="bg-white text-black border-solid border  rounded-md border-[rgba(228,228,231,1)]"
               placeholder="Add a new task..."
             ></input>
-            <button className="bg-[rgba(60,130,246,1)] p-1 rounded-md">
+            <button
+              onClick={() => {
+                addTask();
+                setInputValue("");
+              }}
+              className="bg-[rgba(60,130,246,1)] p-1 rounded-md"
+            >
               Add
             </button>
           </div>
           <div className="flex gap-1.5 self-start ">
             <button
-              onClick={() => pressButton("all")}
-              className={`p-1 rounded-md px-5 ${pressedButton == "all" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
+              onClick={() => changeFilter("all")}
+              className={`p-1 rounded-md px-5 ${filter == "all" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
             >
               All
             </button>
             <button
-              onClick={() => pressButton("active")}
-              className={`p-1 rounded-md px-5 ${pressedButton == "active" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
+              onClick={() => changeFilter("active")}
+              className={`p-1 rounded-md px-5 ${filter == "active" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
             >
               Active
             </button>
             <button
-              onClick={() => pressButton("completed")}
-              className={`p-1 rounded-md px-5 ${pressedButton == "completed" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
+              onClick={() => changeFilter("completed")}
+              className={`p-1 rounded-md px-5 ${filter == "completed" ? "bg-[#3c82f6]" : "bg-[#f3f4f6] "}`}
             >
               Completed
             </button>
           </div>
-          {false ? (
+          {!data.length ? (
             <div className="text-[rgba(107,114,128,1)]">
               No tasks yet. Add one above!
             </div>
           ) : (
-            <List />
+            <div className="flex flex-col w-full gap-2.5">
+              {filteredTasks?.map((task) => (
+                <div
+                  className="flex text-black p-4 w-full  bg-[rgba(249,250,251,1)]  rounded-md items-center justify-between"
+                  key={task.id}
+                >
+                  <div className="flex flex-row items-center gap-2.5">
+                    <div
+                      className={`w-5 h-5  ${task?.completed ? "bg-[#42bf29]" : "bg-white"} border-[#767676] border rounded-xs`}
+                    ></div>
+                    {task.text}
+                  </div>
+                  <button
+                    onClick={() => removeTask(task.id)}
+                    className="text-red-600 bg-red-100 rounded-md px-2 px-1.5"
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
+            </div>
           )}
           <div className="text-[rgba(107,114,128,1)]">
             Powered by
